8aa1bf3719fdac24d7043dad70eff83aa5ac09de
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-04-16 10:16:52 +0430

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace project
+{
+    class test
+    {
+        public static void Main()
+        {
+            Console.WriteLine("inter the Midterm score");
+            double Midterm = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("inter the finale score");
+            double Finale = Convert.ToDouble(Console.ReadLine());
+            double show = score(Midterm, Finale);
+            Console.WriteLine(show);
+        }
+        private static double score(double x, double y)
+        {
+            double Result = 0.35 * x + 0.45 * y;
+            if (Result >= 10)
+            {
+                Console.WriteLine("Pass");
+            }
+            else
+                Console.WriteLine("Failed");
+            return Result;
+        }
+    }
+
+}
